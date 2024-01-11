e527851428bc196137f23b972af5d38b780aec61
Isac.dev <117308223+isacvitor13@users.noreply.github.com>
2024-01-11 11:20:36 -0300

    Add files via upload
    Verificador de idade que verifica sua idade e sexo, dando resultado com imagem e calcula sua idade

---
+++ exercicio 1/Verificador.js
@@ -0,0 +1,46 @@
+function verificar() {
+  var ano_nascimento = document.getElementById('ano_nascimento')
+  var anos = new Date()
+  var ano_atual = anos.getFullYear()
+  var resultado = document.getElementById('resultado')
+
+  if (ano_nascimento.value.length == 0 || ano_nascimento.value >= ano_atual) {
+    resultado.style.color = 'red'
+    resultado.innerHTML = '<p>[ERRO!!!] Verifique os dados e tente novamente!</p>'
+  } else {
+    resultado.style.color = 'green'
+    var sexo = document.getElementsByName('radsex')
+    var idade_pessoa = ano_atual - ano_nascimento.value
+    var comS = (idade_pessoa > 1) ? 'anos' : 'ano'
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (sexo[0].checked) {
+      genero = 'Mulher'
+      if (idade_pessoa > 0 && idade_pessoa <= 10) {
+        img.setAttribute('src', '/verificador de idade/imagens/criançamenina.jpg')
+      } else if (idade_pessoa < 18) {
+        img.setAttribute('src', '/verificador de idade/imagens/menina.jpg')
+      } else if (idade_pessoa >= 18 && idade_pessoa < 65) {
+        img.setAttribute('src', '/verificador de idade/imagens/mulher.jpg')
+      } else if (idade_pessoa >= 65) {
+        img.setAttribute('src', '/verificador de idade/imagens/idosa.jpg')
+      }
+    } else if (sexo[1].checked) {
+      genero = 'Homem'
+      if (idade_pessoa > 0 && idade_pessoa <= 10) {
+        img.setAttribute('src', '/verificador de idade/imagens/criançamenino.jpg')
+      } else if (idade_pessoa < 18) {
+        img.setAttribute('src', '/verificador de idade/imagens/menino.jpg')
+      } else if (idade_pessoa >= 18 && idade_pessoa < 65) {
+        img.setAttribute('src', '/verificador de idade/imagens/homem.jpg')
+      } else if (idade_pessoa >= 65) {
+        img.setAttribute('src', '/verificador de idade/imagens/idoso.jpg')
+      }
+    }
+    resultado.style.textAlign = 'center'
+    resultado.innerHTML = `<p>Detectamos ${genero} com ${idade_pessoa} ${comS}</p>`
+    resultado.appendChild(img)
+
+  }
+}
